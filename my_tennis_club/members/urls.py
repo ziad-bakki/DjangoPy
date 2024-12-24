16e460db140a3fb40359031ad61bfeb7f0ce4c8f
ziad-bakki <bakkizia@msu.edu>
2024-12-24 19:32:03 +0300

    added template files and database handling

--- my_tennis_club/members/urls.py
+++ my_tennis_club/members/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.main, name='main'),
     path('', views.members, name='members'),
+    path('members', views.members, name='members'),
+    path('details/<int:id>', views.details, name='details')
+
 ]
--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-&u+*@1_bz+-2h!2z129i&u0io8nz2@-v!bqugdbtnjxbtk+9$!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
